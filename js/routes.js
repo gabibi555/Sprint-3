391c64dedc88bf049befd6c1921ad1bad4d921c3
Gabi Chami <thegabibi@gmail.com>
2018-11-06 19:18:24 +0200

    added routes

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 'use strict';
 
-import homePage from '../pages/home-page.js';
+import homePage from './pages/home-page.js';
+import aboutPage from './pages/about-page.js';
 
 var myRoutes = [
     {path: '/home', component: homePage },
+    {path: '/about', component: aboutPage },
 ]
 
 export default myRoutes;
