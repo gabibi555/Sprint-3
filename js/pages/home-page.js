797400691922c101d967d76071af4465f251cfba
Gabi Chami <thegabibi@gmail.com>
2018-11-11 00:33:33 +0200

    home-page

--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -3,7 +3,22 @@
 export default {
     template: `
         <section class="home-page" >
-        home page
+            <div class="home-title">
+                 Welcome to AppSus
+            </div>
+          <div class="big-btns-container">
+              <div class="go-to-email">
+                  <div class="go-to-emailtxt">
+                      To emailVile
+                  </div>
+              </div>
+              <div class="go-to-note">
+                  <div class="go-to-emailtxt">
+                      To noteVile
+                  </div>
+              </div>
+              </div>
+          </div>
         </section>
     `,
     data() {
